4dc3b6066575ab4f6fd86578f9d008173a55320f
zhenghui0705 <648338777@qq.com>
2018-04-14 23:14:35 +0800

    update

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,16 +2,30 @@ require('normalize.css/normalize.css');
 require('styles/App.css');
 
 import React from 'react';
+// 引入图片json数据
+var imagesDatas = require('../data/images.json')
 
-let yeomanImage = require('../images/yeoman.png');
+//利用自执行函数，将图片信息转换成URL路径信息
+var imagesDatas = (function genImageURL(imageDataArr){
+	for(var i = 0, j = imageDataArr.length;i < j; i++){
+		var singleImageData = imageDataArr[i];
+		singleImageData.imageURL = require('../images/' + singleImageData.fileName);
+		imageDataArr[i] = singleImageData;
+	}
+	return imageDataArr
+})(imagesDatas);
 
 class AppComponent extends React.Component {
   render() {
     return (
-      <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
-      </div>
+		<section className="stage">
+			<section className="img-sec">
+			</section>
+			<nav className="controller-nav">
+			
+			</nav>
+		</section>
+	
     );
   }
 }
